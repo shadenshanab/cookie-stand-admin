2b2d2c91e8885db394aeec57aa583aa3868c6ccb
Shaden Shanab <shadenshanab2@gmail.com>
2023-01-08 07:41:32 +0000

    dark mode with err in header

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "./header";
 import { useState } from "react";
 import ReportTable from './ReportTable'
-
+import { useContext } from 'react';
+import {ThemeContext} from './contexts/theme'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Form() {
   const [userInput, setUserInput] = useState({
@@ -30,12 +33,12 @@ export default function Form() {
 
   return (
     <>
-
-      <section className="bg-[#251740] py-4 px-4  text-center ">
+      <Header/>
+      <section className="py-4 px-4  text-center ">
       <form className="m-6" onSubmit={submitHandler}>
-          <label className="block font-bold text-2xl mb-2 text-[#B08BBB]"> Cookies Stand </label>
+          <label className="block font-bold text-2xl mb-2 text-[#000000]"> Cookies Stand </label>
           <br></br>
-          <label className="block font-bold font-[] text-xl mb-2 text-[#B08BBB]"> Location </label>
+          <label className="block font-bold font-[] text-xl mb-2 text-[#000000]"> Location </label>
           <input
             onChange={handleChange}
             placeholder="Location"
@@ -45,7 +48,7 @@ export default function Form() {
           <br></br>
           <div className="flex justify-between mt-7">
             <div className="w-1/3">
-              <label className="block font-bold text-xl mb-2 text-[#B08BBB]"> Maximum Customers per hour </label>
+              <label className="block font-bold text-xl mb-2 text-[#000000]"> Maximum Customers per hour </label>
               <input
                 onChange={handleChange}
                 placeholder="Maximum"
@@ -54,7 +57,7 @@ export default function Form() {
               />
             </div>
             <div className="w-1/3">
-              <label className="block font-bold text-xl mb-2 text-[#B08BBB]"> Minimum Customers per hour </label>
+              <label className="block font-bold text-xl mb-2 text-[#000000]"> Minimum Customers per hour </label>
               <input
                 onChange={handleChange}
                 placeholder="Minimum"
@@ -63,7 +66,7 @@ export default function Form() {
               />
             </div>
             <div className="w-1/3">
-              <label className="block font-bold text-xl mb-2 text-[#B08BBB]"> Average Cookies per sale </label>
+              <label className="block font-bold text-xl mb-2 text-[#000000]"> Average Cookies per sale </label>
               <input
                 onChange={handleChange}
                 placeholder="Average"
@@ -78,26 +81,20 @@ export default function Form() {
           </button>
         </form>
       </section>
-      <section className="w-full pt-10 text-center shadow-lg shadow-black-50 text-xl bg-[#495579] text-[#FFFBEB] ">
-        {fullReplyState.length > 0 &&
-      <section className="w-full pt-10 text-center shadow-lg shadow-black-50 text-xl bg-[#84563C] text-[#EFE2B2] pb-10">
-      {/* {fullReplyState.length > 0 &&
-        fullReplyState.map((item, index) => {
-          return(
-            <p className="py-2">{JSON.stringify(item)}</p>
-          );
-        })}
-        })} */}
-                         {fullReplyState.length > 0 && <ReportTable 
+      <section className="w-full pt-10 text-center shadow-lg shadow-black-50 text-xl bg-[#263159] text-[#EFE2B2] pb-10">
+        {/* {fullReplyState.length > 0 &&
+          fullReplyState.map((item, index) => {
+            return(
+              <p className="py-2">{JSON.stringify(item)}</p>
+            );
+          })}
+          })} */}
+                 {fullReplyState.length > 0 && <ReportTable 
                 report = {fullReplyState}
                 />}
                 {fullReplyState.length == 0 && <h2>No Cookie Stands Available</h2>}
 
       </section>
-  
-
-
-
-    </>
+  </>
   );
 }
---
+++ pages/layout.jsx
@@ -0,0 +1,34 @@
+'use client';
+
+
+import './globals.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import ThemeWrapper from './contexts/theme'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <ThemeWrapper>
+      <body>
+        <header>
+          <Header />
+        </header>
+        <main className='dark:bg-[#BD8C61]'
+>
+          {children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+        </body>
+        </ThemeWrapper>
+
+    </html>
+  )
+}
